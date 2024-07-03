80f5342958a0279b8f5b279722915b8a6d3ca604
Alexandre Zamberlan <alexandre.o.zamberlan@gmail.com>
2024-07-03 15:35:34 -0300

    banco refeito no local

--- projeto/projeto/settings.py
+++ projeto/projeto/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '=%6aqk0p^aux0qvolqn_7efyj(@wh*wtc_!n10u8_o4!l#k6)h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.saoa.lapinf.ufn.edu.br','saoa.lapinf.ufn.edu.br']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', 'www.saoa.lapinf.ufn.edu.br','saoa.lapinf.ufn.edu.br']
 
 AUTH_USER_MODEL = 'usuario.Usuario'
 
